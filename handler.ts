a9acb9304e6445c5081bf856fd8104061e450c1a
miquelnez <miquelnez@gmail.com>
2020-02-16 09:33:16 +0100

    end of ideas

--- handler.ts
+++ handler.ts
@@ -1,21 +1,26 @@
 import { APIGatewayProxyHandler } from "aws-lambda";
 import "source-map-support/register";
-import { loggerTechnical, loggerAnonymized } from "./logger";
+import { loggerTechnical } from "./logger";
+// import { loggerTechnical, loggerAnonymized } from "./logger";
 
 export const hello: APIGatewayProxyHandler = async (event, _context) => {
+
+  process.on("exit", function() {
+    console.log(">> Process is exiting");
+  });
+
   loggerTechnical.log({
     level: "info",
     message: "Hello has a new loggerTechnical log request"
   });
 
-  loggerAnonymized.log({
-    level: "info",
-    message: "Hello has a new loggerAnonymized log request"
-  });
+//   loggerAnonymized.log({
+//     level: "info",
+//     message: "Hello has a new loggerAnonymized log request"
+//   });
 
-//   loggerTechnical.end();
+  loggerTechnical.end();
 //   loggerAnonymized.end();
-
 
   return {
     statusCode: 200,
--- logger.ts
+++ logger.ts
@@ -6,66 +6,71 @@ import moment from "moment";
 import { get } from "lodash";
 
 const { combine, timestamp, printf } = format;
-// AWS.config.update({
-//   region: get(process.env, 'CLOUD_WATCH_REGION')
-// });
+AWS.config.update({
+  region: get(process.env, 'CLOUD_WATCH_REGION')
+});
 
 const technicalFormat = printf(({ level, message, timestamp }) => {
   return `${timestamp} *technical* [${level.toUpperCase()}]: ${message}`;
 });
-const anonymizedFormat = printf(({ level, message, timestamp }) => {
-  return `${timestamp} *anonymized* [${level.toUpperCase()}]: ${message}`;
-});
+// const anonymizedFormat = printf(({ level, message, timestamp }) => {
+//   return `${timestamp} *anonymized* [${level.toUpperCase()}]: ${message}`;
+// });
 
 winston.loggers.add("technical", {
   format: combine(timestamp(), technicalFormat),
   transports: [
     new winston.transports.Console(),
-    // new WinstonCloudWatch({
-    //   cloudWatchLogs: new AWS.CloudWatchLogs(),
-    //   logGroupName: "sls-aws-playground-TECHNICAL",
-    //   logStreamName: moment().format("L")
-    //     awsAccessKeyId: get(process.env, 'CLOUD_WATCH_IAM_KEY_ID'),
-    //     awsSecretKey: get(process.env, 'CLOUD_WATCH_IAM_ACCESS_KEY'),
-    //     awsRegion: "us-east-1"
-    // })
+    new WinstonCloudWatch({
+      cloudWatchLogs: new AWS.CloudWatchLogs(),
+      logGroupName: "sls-aws-playground-TECHNICAL",
+      logStreamName: moment().format("L"),
+        awsAccessKeyId: get(process.env, 'CLOUD_WATCH_IAM_KEY_ID'),
+        awsSecretKey: get(process.env, 'CLOUD_WATCH_IAM_ACCESS_KEY'),
+        awsRegion: get(process.env, 'CLOUD_WATCH_REGION')
+    })
   ]
 });
-winston.loggers.add("anonymized", {
-  format: combine(timestamp(), anonymizedFormat),
-  transports: [
-    new winston.transports.Console(),
-    // new WinstonCloudWatch({
-    //   cloudWatchLogs: new AWS.CloudWatchLogs(),
-    //   logGroupName: "sls-aws-playground-ANONYMIZED",
-    //   logStreamName: moment().format("L")
-    // //   awsAccessKeyId: get(process.env, 'CLOUD_WATCH_IAM_KEY_ID'),
-    // //   awsSecretKey: get(process.env, 'CLOUD_WATCH_IAM_ACCESS_KEY'),
-    // //   awsRegion: "us-east-1"
-    // })
-  ]
+// winston.loggers.add("anonymized", {
+//   format: combine(timestamp(), anonymizedFormat),
+//   transports: [
+//     new winston.transports.Console(),
+//     new WinstonCloudWatch({
+//       cloudWatchLogs: new AWS.CloudWatchLogs(),
+//       logGroupName: "sls-aws-playground-ANONYMIZED",
+//       logStreamName: moment().format("L"),
+//       awsAccessKeyId: get(process.env, 'CLOUD_WATCH_IAM_KEY_ID'),
+//       awsSecretKey: get(process.env, 'CLOUD_WATCH_IAM_ACCESS_KEY'),
+//       awsRegion: get(process.env, 'CLOUD_WATCH_REGION')
+//     })
+//   ]
+// });
+
+winston.loggers.get("technical").on('finish', function () {
+    console.log('technical logger is done logging');
+    process.exit(0);
+});
+winston.loggers.get("technical").on('error', function (err) {
+    console.log('technical logger is broken', err);
+    process.exit(0);
 });
 
-// process.on("exit", function() {
-//   console.log("Your process is exiting");
-// });
-
-// winston.loggers.get("technical").on('finish', function () {
-//     console.log('technical logger is done logging');
-// });
-// winston.loggers.get("technical").on('error', function (err) {
-//     console.log('technical logger is broken', err);
-// });
-
-// winston.loggers.get("technical").on('finish', function () {
-//     console.log('technical logger is done logging');
-// });
 // winston.loggers.get("anonymized").on('finish', function () {
-//     console.log('anonymized logger is done logging');
+//     console.log('technical logger is done logging');
 // });
+// winston.loggers.get("anonymized").on('error', function (err) {
+//     console.log('anonymized logger is broken', err);
+// });
+
+winston.loggers.get("technical").on('flush', function() {
+    process.exit(0);
+});
+
+console.log('technical transports', winston.loggers.get("technical").transports);
+
 
 export const loggerTechnical = winston.loggers.get("technical");
-export const loggerAnonymized = winston.loggers.get("anonymized");
+// export const loggerAnonymized = winston.loggers.get("anonymized");
 
 // loggerTechnical.on('finish', function (info) {
 //     console.log('loggerTechnical finished');
@@ -75,3 +80,4 @@ export const loggerAnonymized = winston.loggers.get("anonymized");
 //     console.log('loggerTechnical finished');
 //     this.end();
 // });
+
