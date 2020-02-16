5ba2e2c267bc91b4f58e2025cd830bb515b8e755
miquelnez <miquelnez@gmail.com>
2020-02-16 13:18:14 +0100

    waitForIt

--- winston-logger.ts
+++ winston-logger.ts
@@ -9,16 +9,17 @@
 import { get } from "lodash";
 import moment from "moment";
 import winston from "winston";
-import { format } from "winston";
+// import { format } from "winston";
 import CloudWatchTransport from "winston-aws-cloudwatch";
 
-const { combine, timestamp, printf } = format;
-const technicalFormat = printf(({ level, message, timestamp }) => {
-  return `${timestamp} *technical* [${level.toUpperCase()}]: ${message}`;
-});
-const anonymizedFormat = printf(({ level, message, timestamp }) => {
-  return `${timestamp} *anonymized* [${level.toUpperCase()}]: ${message}`;
-});
+// const { combine, timestamp, printf } = format;
+// const technicalFormat = printf(({ level, message, timestamp }) => {
+//   return `${timestamp} *technical* [${level.toUpperCase()}]: ${message}`;
+// });
+// const anonymizedFormat = printf(({ level, message, timestamp }) => {
+//   return `${timestamp} *anonymized* [${level.toUpperCase()}]: ${message}`;
+// });
+const now: string = moment().format("L");
 
 export const winstonLogger = winston.createLogger({
   transports: [
@@ -36,33 +37,33 @@ export const winstonLogger = winston.createLogger({
         secretAccessKey: get(process.env, "CLOUD_WATCH_IAM_ACCESS_KEY"),
         region: get(process.env, "CLOUD_WATCH_REGION")
       },
-        formatLog: function(item) {
-          return (
-            `${moment().format('L')} *anonymized* [${item.level.toUpperCase()}]: ${item.message}`
-          );
-        }
+      formatLog: function(item) {
+        return `${now} *anonymized* [${item.level.toUpperCase()}]: ${
+          item.message
+        }`;
+      }
       //   formatLog: combine(timestamp(), anonymizedFormat)
-    }),
-    new CloudWatchTransport({
-      logGroupName: "sls-aws-playground-TECHNICAL",
-      logStreamName: moment().format("L"),
-      createLogGroup: true,
-      createLogStream: true,
-      submissionInterval: 1000,
-      submissionRetryCount: 1,
-      //   batchSize: 20,
-      awsConfig: {
-        accessKeyId: get(process.env, "CLOUD_WATCH_IAM_KEY_ID"),
-        secretAccessKey: get(process.env, "CLOUD_WATCH_IAM_ACCESS_KEY"),
-        region: get(process.env, "CLOUD_WATCH_REGION")
-      },
-      //   formatLog: function(item) {
-      //     return (
-      //       item.level + ": " + item.message + " " + JSON.stringify(item.meta)
-      //     );
-      //   }
-      formatLog: combine(timestamp(), technicalFormat)
     })
+    // new CloudWatchTransport({
+    //   logGroupName: "sls-aws-playground-TECHNICAL",
+    //   logStreamName: moment().format("L"),
+    //   createLogGroup: true,
+    //   createLogStream: true,
+    //   submissionInterval: 1000,
+    //   submissionRetryCount: 1,
+    //   //   batchSize: 20,
+    //   awsConfig: {
+    //     accessKeyId: get(process.env, "CLOUD_WATCH_IAM_KEY_ID"),
+    //     secretAccessKey: get(process.env, "CLOUD_WATCH_IAM_ACCESS_KEY"),
+    //     region: get(process.env, "CLOUD_WATCH_REGION")
+    //   },
+    //     formatLog: function(item) {
+    //       return (
+    //         item.level + ": " + item.message + " " + JSON.stringify(item.meta)
+    //       );
+    //     }
+    // //   formatLog: combine(timestamp(), technicalFormat)
+    // })
   ]
 });
 
@@ -86,10 +87,21 @@ export async function lambdaWinstonLogger(
 
 async function waitForLogger(logger) {
   console.log("waitForLogger");
-  const loggerDone = new Promise(resolve => logger.on("finish", resolve));
+  const loggerDone = new Promise(resolve => {
+    logger.on("finish", resolve);
+  });
+
+  const waitForIt = new Promise(resolve => {
+    console.log("Starting waitForIt");
+    setTimeout(() => {
+      console.log("Finished waiting");
+      resolve();
+    }, 200);
+  });
+
   // alternatively, use end-of-stream https://www.npmjs.com/package/end-of-stream
   // although I haven't tested this
   // const loggerDone = new Promise(resolve => eos(logger, resolve));
-  logger.end();
-  return loggerDone;
+  //   logger.end();
+  return loggerDone && waitForIt;
 }
